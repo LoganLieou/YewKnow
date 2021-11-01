e5a2fd1fa6cf46fa125c87196ce45d8ae9bd8a70
Logan <logan.lieou@protonmail.com>
2021-11-01 02:16:12 -0500

    no time

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,124 @@
-use yew::prelude::*;
+use std::collections::HashMap;
 
-enum Msg {
-    AddOne,
+use web_sys::{Event, HtmlInputElement};
+use yew::{html, html::TargetCast, Component, Context, Html};
+
+use gloo_file::callbacks::FileReader;
+use gloo_file::File;
+
+type Chunks = bool;
+
+pub enum Msg {
+    Loaded(String, String),
+    LoadedBytes(String, Vec<u8>),
+    Files(Vec<File>, Chunks),
+    ToggleReadBytes,
 }
 
-struct Model {
-    link: ComponentLink<Self>,
-    value: i64,
+pub struct Model {
+    readers: HashMap<String, FileReader>,
+    files: Vec<String>,
+    read_bytes: bool,
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+    fn create(_ctx: &Context<Self>) -> Self {
         Self {
-            link,
-            value: 0,
+            readers: HashMap::default(),
+            files: vec![],
+            read_bytes: false,
         }
     }
 
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
+            Msg::Loaded(file_name, data) => {
+                let info = format!("file_name: {}, data: {:?}", file_name, data);
+                self.files.push(info);
+                self.readers.remove(&file_name);
+                true
+            }
+            Msg::LoadedBytes(file_name, data) => {
+                let info = format!("file_name: {}, data: {:?}", file_name, data);
+                self.files.push(info);
+                self.readers.remove(&file_name);
+                true
+            }
+            Msg::Files(files, bytes) => {
+                for file in files.into_iter() {
+                    let file_name = file.name();
+                    let task = {
+                        let file_name = file_name.clone();
+                        let link = ctx.link().clone();
+
+                        if bytes {
+                            gloo_file::callbacks::read_as_bytes(&file, move |res| {
+                                link.send_message(Msg::LoadedBytes(
+                                    file_name,
+                                    res.expect("failed to read file"),
+                                ))
+                            })
+                        } else {
+                            gloo_file::callbacks::read_as_text(&file, move |res| {
+                                link.send_message(Msg::Loaded(
+                                    file_name,
+                                    res.unwrap_or_else(|e| e.to_string()),
+                                ))
+                            })
+                        }
+                    };
+                    self.readers.insert(file_name, task);
+                }
+                true
+            }
+            Msg::ToggleReadBytes => {
+                self.read_bytes = !self.read_bytes;
                 true
             }
         }
     }
 
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        false
-    }
-
-    fn view(&self) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let flag = self.read_bytes;
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "1+" }</button>
-                <p>{ self.value }</p>
+                <div>
+                    <p>{ "Choose a file to upload to see the uploaded bytes" }</p>
+                    <input type="file" multiple=true onchange={ctx.link().callback(move |e: Event| {
+                            let mut result = Vec::new();
+                            let input: HtmlInputElement = e.target_unchecked_into();
+
+                            if let Some(files) = input.files() {
+                                let files = js_sys::try_iter(&files)
+                                    .unwrap()
+                                    .unwrap()
+                                    .map(|v| web_sys::File::from(v.unwrap()))
+                                    .map(File::from);
+                                result.extend(files);
+                            }
+                            Msg::Files(result, flag)
+                        })}
+                    />
+                </div>
+                <div>
+                    <label>{ "Read bytes" }</label>
+                    <input type="checkbox" checked={flag} onclick={ctx.link().callback(|_| Msg::ToggleReadBytes)} />
+                </div>
+                <ul>
+                    { for self.files.iter().map(|f| Self::view_file(f)) }
+                </ul>
             </div>
+        }
+    }
+}
+
+impl Model {
+    fn view_file(data: &str) -> Html {
+        html! {
+            <li>{ data }</li>
         }
     }
 }
